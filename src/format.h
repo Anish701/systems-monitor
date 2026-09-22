bc5f902ecb8d125c0c29395e1f45835f43085029
Anish701 <anish2sinha@gmail.com>
2026-09-22 14:17:03 -0400

    feat: refactor
    Signed-off-by: Anish701 <anish2sinha@gmail.com>

--- src/format.h
+++ src/format.h
@@ -1,3 +1,6 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
 #include <string>
 
 #include "monitor_usage.h"
@@ -5,3 +8,5 @@
 std::string percentage(float fraction);
 
 void print_usage(UsageData cpu_usage_data, UsageData memory_usage_data);
+
+#endif
