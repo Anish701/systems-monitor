d1ba3893b3e84e49a0cf625c07fc05513ebfef2f
Anish701 <anish2sinha@gmail.com>
2026-09-22 14:14:14 -0400

    feat: refactor
    Signed-off-by: Anish701 <anish2sinha@gmail.com>

--- src/format.cpp
+++ src/format.cpp
@@ -10,6 +10,8 @@ string percentage(float fraction) {
 }
 
 void print_usage(UsageData cpu_usage_data, UsageData memory_usage_data) {
-    cout << "RAM: " << percentage(memory_usage_data.usage_ratio) << endl;
-    cout << "CPU: " << percentage(cpu_usage_data.usage_ratio) << endl;
+    cout << "RAM: " << percentage(memory_usage_data.usage_ratio) << " - " 
+         << memory_usage_data.used_gb << " / " << memory_usage_data.total_gb << endl;
+    cout << "CPU: " << percentage(cpu_usage_data.usage_ratio) << " - " 
+         << cpu_usage_data.used_gb << " / " << cpu_usage_data.total_gb << endl;
 }
